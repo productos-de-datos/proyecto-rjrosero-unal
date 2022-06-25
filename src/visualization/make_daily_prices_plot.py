ea0f7573ee26683a951a2061a9ac1a06fb6a2632
Roberto José Rosero Herrera <101530665+rjrosero-unal@users.noreply.github.com>
2022-06-25 08:23:53 -0500

    Update make_daily_prices_plot.py

--- src/visualization/make_daily_prices_plot.py
+++ src/visualization/make_daily_prices_plot.py
@@ -17,7 +17,7 @@ def make_daily_prices_plot():
     import os
 
     # Importar el archivo de precios diarios
-    os.chdir(os.getcwd() + "/src/data/src")
+    os.chdir(os.getcwd() + "/src/data")
     dir_path = os.path.dirname(os.path.realpath(__file__))
     print(os.getcwd())
     print(dir_path)
